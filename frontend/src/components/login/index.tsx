29f793988e5edf25928fdef90c0b9b3bb7f494a8
Lukas Wuttke <lukas.wuttke2108@outlook.de>
2026-03-04 09:49:51 +0100

    login page

--- frontend/src/components/login/index.tsx
+++ frontend/src/components/login/index.tsx
@@ -1,6 +1,16 @@
+import './index.css';
+
 const LoginScreen = () => {
     return (
-        <div>login/index.tsx</div>
+        <>
+            <div>login/index.tsx</div>
+            <div className="loginWindow">
+                <h1>Login</h1>
+                <input type="text" placeholder="Username" />
+                <input type="password" placeholder="Password" />
+                <button>Login</button>
+            </div>
+        </>
     );
 };
 
