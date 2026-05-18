de9eb3cf5ca2e30ba23aa9aaa34ec1616c3d5130
Wohlgemuth-Dev <wohlgemuth.dev@gmail.com>
2026-05-18 14:52:30 +0200

    fix: local url to host url and cleanup of function error

--- frontend/src/service/api/client.ts
+++ frontend/src/service/api/client.ts
@@ -1,5 +1,5 @@
 
-export const API_BASE_URL = "http://localhost:8080";
+export const API_BASE_URL = window.location.protocol + "//" + window.location.hostname + ":8080";
 
 export class ApiError extends Error {
     public status: number;
